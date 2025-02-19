ee9342b872c1e5ccb2b7a5caa7b243f6820f4797
Eloiza <eloiza.souza@zup.com.br>
2025-02-19 13:26:29 -0300

    feat: create class BookController

---
+++ src/main/java/br_com/zup/library_group/controllers/BookController.java
@@ -0,0 +1,20 @@
+package br_com.zup.library_group.controllers;
+
+import br_com.zup.library_group.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+}
