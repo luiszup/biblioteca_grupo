ef7f44e1d35ead51007f36fb82ba82b40b222453
Eloiza <eloiza.souza@zup.com.br>
2025-02-19 15:47:13 -0300

    feat: add bookService

---
+++ src/main/java/br_com/zup/library_group/services/BookService.java
@@ -0,0 +1,40 @@
+package br_com.zup.library_group.services;
+
+import br_com.zup.library_group.dtos.BookRequest;
+import br_com.zup.library_group.dtos.BookResponse;
+import br_com.zup.library_group.models.Book;
+import br_com.zup.library_group.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public BookResponse addBook(BookRequest bookRequest) {
+
+        Book book = new Book();
+        book.setTitle(bookRequest.title());
+        book.setAuthor(bookRequest.author());
+        book.setPublicationYear(bookRequest.publicationYear());
+        book.setGenre(bookRequest.genre());
+
+        Book savedBook = bookRepository.save(book);
+        return new BookResponse(
+                savedBook.getId(), savedBook.getTitle(), savedBook.getAuthor(), savedBook.getPublicationYear(),
+                savedBook.getGenre()
+        );
+    }
+
+
+}
