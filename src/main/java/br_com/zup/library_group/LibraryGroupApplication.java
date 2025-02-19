21a8971f809a87e905179f1f71981bfac7bb1437
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-19 11:48:02 -0300

    chore: first commit

---
+++ src/main/java/br_com/zup/library_group/LibraryGroupApplication.java
@@ -0,0 +1,13 @@
+package br_com.zup.library_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br_com/zup/library_group/LibraryGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package br_com.zup.library_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
