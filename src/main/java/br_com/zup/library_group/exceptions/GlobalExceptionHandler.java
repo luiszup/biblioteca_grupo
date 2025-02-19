30982ce7653d95148e10d2513cc887f1e214abcf
Eloiza <eloiza.souza@zup.com.br>
2025-02-19 14:02:58 -0300

    fix: change message error to English

--- src/main/java/br_com/zup/library_group/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br_com/zup/library_group/exceptions/GlobalExceptionHandler.java
@@ -43,7 +43,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
         Map<String, String> error = new HashMap<>();
-        error.put("error", "Ocorreu um erro inesperado. Mensagem: " + ex.getMessage() + "Tipo do erro: " + ex.fillInStackTrace());
+        error.put("error", "An unexpected error occurred. Please try again later.");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
