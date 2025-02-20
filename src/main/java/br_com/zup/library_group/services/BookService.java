f9d4a5e86ee4c6eac90ed52c09a2d1b63d31628a
Fabricio John Sullivan <fabricio.sullivan@zup.com.br>
2025-02-20 08:48:13 -0300

    Update BookService.java
    Inserting the get, get(id), put(id) and delete(id) functions in the services layer.
    
    Body for POST AND PUT books in JSON format:
    
    POST (http://localhost:8080/api/books)
    {
      "title": "test",
      "author": "r",
      "publicationYear": 2124,
      "genre": "FICTION"
    }
    
    PUT (http://localhost:8080/api/books/id)
    {
      "title": "test",
      "Author": "r",
      "publicationYear": 1111,
      "genre": "HISTORY"
    }

--- src/main/java/br_com/zup/library_group/services/BookService.java
+++ src/main/java/br_com/zup/library_group/services/BookService.java
@@ -24,18 +24,40 @@ public class BookService {
 
     public BookResponse addBook(BookRequest bookRequest) {
 
-        Book book = new Book();
-        book.setTitle(bookRequest.title());
-        book.setAuthor(bookRequest.author());
-        book.setPublicationYear(bookRequest.publicationYear());
-        book.setGenre(Genre.fromString(bookRequest.genre()));
+        Book addBook = new Book();
+        addBook.setTitle(bookRequest.title());
+        addBook.setAuthor(bookRequest.author());
+        addBook.setPublicationYear(bookRequest.publicationYear());
+        addBook.setGenre(Genre.fromString(bookRequest.genre()));
 
-        Book savedBook = bookRepository.save(book);
-        return new BookResponse(
-                savedBook.getId(), savedBook.getTitle(), savedBook.getAuthor(), savedBook.getPublicationYear(),
-                savedBook.getGenre().toString()
-        );
+        Book savedBook = bookRepository.save(addBook);
+        return new BookResponse(savedBook.getId(), savedBook.getTitle(), savedBook.getAuthor(), savedBook.getPublicationYear(), savedBook.getGenre().toString());
+    }
+    
+    public BookResponse getBookById(Long id) {
+        Optional<Book> boockOptional = bookRepository.findById(id);
+        Book Listingbook = boockOptional.get();
+        return new BookResponse(Listingbook.getId(), Listingbook.getTitle(), Listingbook.getAuthor(), Listingbook.getPublicationYear(), Listingbook.getGenre().toString());
     }
 
+    public List<BookResponse> getAllBooks() {
+        return bookRepository.findAll().stream()
+                .map(getAllBooks -> new BookResponse(getAllBooks.getId(), getAllBooks.getTitle(), getAllBooks.getAuthor(), getAllBooks.getPublicationYear(), getAllBooks.getGenre().toString()))
+                .collect(Collectors.toList());
+    }
 
+    public BookResponse updateBook(Long id, BookResponse bookResponse) {
+        Optional<Book> booksOptional = bookRepository.findById(id);
+        Book updateBook = booksOptional.get();
+        updateBook.setTitle(bookResponse.title());
+        updateBook.setAuthor(bookResponse.Author());
+        updateBook.setPublicationYear(bookResponse.publicationYear());
+        updateBook.setGenre(Genre.fromString(bookResponse.genre()));
+        Book savedBook = bookRepository.save(updateBook);
+        return new BookResponse(savedBook.getId(), savedBook.getTitle(), savedBook.getAuthor(), savedBook.getPublicationYear(), savedBook.getGenre().toString());
+    }
+
+    public void deleteBook(long id) {
+        bookRepository.deleteById(id);
+    }
 }
